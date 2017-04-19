b4cdca2549c2f4363a4c65fdb4a3a46db580a2d5
Archangel <radishikerov@yahoo.com>
2017-04-19 11:15:42 +0300

    Add share action Add action picker action Add select contact action Add permission check

--- Example/app/src/main/java/com/example/radi/example/MainActivity.java
+++ Example/app/src/main/java/com/example/radi/example/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.radi.example;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,5 +50,44 @@ public class MainActivity extends AppCompatActivity {
     public void onShowSave(View view) {
         Intent intent = new Intent(this, SaveDataActivity.class);
         startActivity(intent);
+    }
+
+    public void onShowShareOptions(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        String title = getResources().getString(R.string.text_share_actions);
+        Intent chooser = Intent.createChooser(intent, title);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
+    }
+
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
+
+    public void onShowPickContact(View view) {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                System.out.println("Contact selected ");
+            }
+        }
+    }
+
+    public void onCheckPermission(View view) {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            System.out.println(" Permission granted! ");
+        }
+        else if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            System.out.println(" Permission denied! ");
+        }
+
     }
 }
