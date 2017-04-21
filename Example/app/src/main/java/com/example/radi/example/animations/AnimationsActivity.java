5bac17d10c9ab03b9c198ca90c13ce6ac5821c32
Archangel <radishikerov@yahoo.com>
2017-04-21 14:37:11 +0300

    Add cross-fade animation.

--- Example/app/src/main/java/com/example/radi/example/animations/AnimationsActivity.java
+++ Example/app/src/main/java/com/example/radi/example/animations/AnimationsActivity.java
@@ -1,13 +1,18 @@
 package com.example.radi.example.animations;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Fade;
 import android.transition.Scene;
 import android.transition.Transition;
 import android.transition.TransitionManager;
+import android.transition.Visibility;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.radi.example.R;
 
@@ -44,5 +49,52 @@ public class AnimationsActivity extends AppCompatActivity {
         }
     }
 
+    private ScrollView scrollView;
+    private TextView textView;
+
+    public void onCrossFade(View view) {
+        scrollView = (ScrollView) findViewById(R.id.svContent);
+        textView = (TextView) findViewById(R.id.tvScene1);
+        int animationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+        if (scrollView.getVisibility() == View.GONE) {
+            scrollView.setAlpha(0f);
+            scrollView.setVisibility(View.VISIBLE);
+
+            scrollView.animate()
+                    .alpha(1f)
+                    .setDuration(animationDuration)
+                    .setListener(null);
+
+            textView.animate()
+                    .alpha(0f)
+                    .setDuration(animationDuration)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            textView.setVisibility(View.GONE);
+                        }
+                    });
+        }
+        else {
+            textView.setAlpha(0f);
+            textView.setVisibility(View.VISIBLE);
+
+            textView.animate()
+                    .alpha(1f)
+                    .setDuration(animationDuration)
+                    .setListener(null);
+
+            scrollView.animate()
+                    .alpha(0f)
+                    .setDuration(animationDuration)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            scrollView.setVisibility(View.GONE);
+                        }
+                    });
+        }
+    }
 
 }
