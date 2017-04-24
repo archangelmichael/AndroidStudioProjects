767335f3e5d8e6ea8bf09f1c0a580ee9590b8b8e
Archangel <radishikerov@yahoo.com>
2017-04-24 17:17:07 +0300

    Implement actions on row item selected.

--- RayTraining/app/src/main/java/com/example/radi/raytraining/MainActivity.java
+++ RayTraining/app/src/main/java/com/example/radi/raytraining/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.radi.raytraining;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,8 +23,34 @@ public class MainActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.tutorials_list_view);
 
-        final ArrayList<Tutorial> tutorials = Tutorial.getAvailableTutorials();
-        TutorialAdapter adapter = new TutorialAdapter(this, tutorials);
+        final ArrayList<Tutorial> tutorialList = Tutorial.getAvailableTutorials();
+        TutorialAdapter adapter = new TutorialAdapter(this, tutorialList);
         mListView.setAdapter(adapter);
+
+        final Context context = this;
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Tutorial selectedTutorial = tutorialList.get(position);
+                onRowSelected(selectedTutorial, 0);
+            }
+        });
+    }
+
+    public void onRowSelected(Tutorial tutorial, int position) {
+        switch (position) {
+            case 0:
+                Toast.makeText(this, "Fortunes are coming next", Toast.LENGTH_SHORT).show();
+                break;
+            case 1:
+//                Intent detailIntent = new Intent(context, RecipeDetailActivity.class);
+//                detailIntent.putExtra("title", selectedRecipe.title);
+//                detailIntent.putExtra("url", selectedRecipe.instructionUrl);
+//                startActivity(detailIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
