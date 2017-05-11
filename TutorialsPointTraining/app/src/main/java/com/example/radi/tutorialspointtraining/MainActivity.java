79c7e15d9f20f183debff2356cecc9ec48d69085
Archangel <radishikerov@yahoo.com>
2017-05-11 11:46:04 +0300

    Create tutorial following tutorialpoints

---
+++ TutorialsPointTraining/app/src/main/java/com/example/radi/tutorialspointtraining/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.radi.tutorialspointtraining;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.radi.tutorialspointtraining.activity.BasicActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] mTopics = {
+            "Activity Lifecycle"
+    };
+
+    ListView mTopicsListView;
+    ArrayAdapter<String> mTopicsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTopicsListView = (ListView) findViewById(R.id.lv_topics);
+        mTopicsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTopics);
+        mTopicsListView.setAdapter(mTopicsAdapter);
+        mTopicsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onTopicSelected(position);
+            }
+        });
+    }
+
+    private void onTopicSelected(int topicPosition) {
+        switch (topicPosition) {
+            case 0:
+                Intent basicActivityIntent = new Intent(this, BasicActivity.class);
+                startActivity(basicActivityIntent);
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ TutorialsPointTraining/app/src/main/java/com/example/radi/tutorialspointtraining/activity/BasicActivity.java
@@ -0,0 +1,68 @@
+package com.example.radi.tutorialspointtraining.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import com.example.radi.tutorialspointtraining.R;
+
+public class BasicActivity extends AppCompatActivity {
+
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic);
+        Log.d(msg, "The onCreate() event");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_basic_activity);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+}
